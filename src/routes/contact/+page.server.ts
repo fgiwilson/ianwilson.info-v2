31b3cc305844d6dcac0fc87193280e371e594a77
Ian Wilson (Formative Games) <iwilson@formativegames.com>
2025-07-07 11:57:11 -0700

    update +page.server.ts
    hard-coded email address as user ID.

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -10,8 +10,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    user: process.env.EMAIL_USER || '', // Replace with your email
-    pass: process.env.EMAIL_PASS || '', // Replace with your email password or app password
+    user: 'ian@ianwilson.info', // Replace with your email
+    pass: process.env.EMAIL_PASS, // Replace with your email password or app password
   },
 });
 
