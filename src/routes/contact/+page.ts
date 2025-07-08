c0cd9b22224dfc76286e7451be635a4b1534aae3
Ian Wilson (Formative Games) <iwilson@formativegames.com>
2025-07-08 16:00:28 -0700

    use process.env instead of $env/static.

--- src/routes/contact/+page.ts
+++ src/routes/contact/+page.ts
@@ -1,12 +1,11 @@
 // Import the public environment variable safely
-import { PUBLIC_TURNSTILE_SITE_KEY } from '$env/static/public';
 
 export function load() {
     // Access the environment variable directly
     // In production, this will be the value from the server environment
     // In development, this will be from your .env file
     // In CI without the variable, it will be undefined but won't crash
-    const siteKey = PUBLIC_TURNSTILE_SITE_KEY || '';
+    const siteKey = process.env.PUBLIC_TURNSTILE_SITE_KEY || '';
     
     return {
         siteKey
